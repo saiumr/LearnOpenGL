b74cbf5e0553082bd0911e730e4b0288f159cc44
saiumr <colaloveanan@gmail.com>
2024-11-22 11:08:33 +0800

    mod: init content of tutorial.

--- OpenGL7-Color/shader.fs
+++ OpenGL7-Color/shader.fs
@@ -8,9 +8,12 @@ uniform sampler2D texture0;
 uniform sampler2D texture1;
 uniform float blend;
 
+vec3 LightingColor = vec3(1.0f, 1.0f, 1.0f);
+vec3 StuffColor = vec3(1.0f, 0.5f, 0.31f);
+
 void main()
 {
     // practice1: look another side
-    FragColor = mix(texture(texture0, TexCoord), texture(texture1, vec2(1.0 - TexCoord.x, TexCoord.y)), blend);
-
+    // FragColor = mix(texture(texture0, TexCoord), texture(texture1, vec2(1.0 - TexCoord.x, TexCoord.y)), blend);
+    FragColor = vec4(LightingColor * StuffColor, 1.0f);
 }
