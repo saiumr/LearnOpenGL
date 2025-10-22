363a073f5045d124724df70d383557594de47537
saiumr <colaloveanan@gmail.com>
2025-10-22 17:27:29 +0800

    gl26-Advanced Lighting: Blinn-Phong lighting model.

---
+++ OpenGL26-BlinnPhong/main.cpp
@@ -0,0 +1,236 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <algorithm>
+#include <map>
+#include <memory>
+#include "vertex.h"
+#include "shader.h"
+#include "camera.h"
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+int  InitWindow();
+void RenderLoop();
+
+unsigned int LoadTexture(const char* file_path);
+void ProcessInput(GLFWwindow* window);
+void FramebufferSizeCallback(GLFWwindow* window, int width, int height);
+void MouseCallback(GLFWwindow* window, double xposIn, double yposIn);
+void ScrollCallback(GLFWwindow* window, double xoffset, double yoffset);
+
+GLFWwindow* window { nullptr };
+const int kScreenWidth  { 800 };
+const int kScreenHeight { 600 };
+
+Camera camera { glm::vec3 {0.0f, 0.0f, 5.0f} };
+bool  first_mouse { true };
+float last_x { static_cast<float>(kScreenWidth) / 2.0f };
+float last_y { static_cast<float>(kScreenHeight) / 2.0f };
+float delta_time { 0.0f };
+float last_frame { 0.0f };
+
+bool blinn { false };
+
+int main(int argc, const char** argv) {
+	if (InitWindow() < 0) {
+		return -1;
+	}
+
+	RenderLoop();
+	glfwTerminate();
+
+	return 0;
+}
+
+
+//////////////////////////////////////////////////////////////
+
+
+void RenderLoop() {
+	Vertex vertex;
+	unsigned int floor_texture { LoadTexture("wood.jpg") };
+	Shader shader { "advanced_lighting.vert", "advanced_lighting.frag" };
+
+	shader.use();
+	shader.setInt("floor_texture", 0);
+
+	glm::vec3 light_pos { 0.0f, 0.0f, 0.0f };
+
+	while (!glfwWindowShouldClose(window)) {
+		float current_frame = static_cast<float>(glfwGetTime());
+		delta_time = current_frame - last_frame;
+		last_frame = current_frame;
+
+		ProcessInput(window);
+
+		glClearColor(0.15f, 0.16f, 0.18f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		glm::mat4 model{ 1.0f };
+		glm::mat4 view{ camera.GetViewMatrix() };
+		glm::mat4 projection{ glm::perspective(glm::radians(45.0f), static_cast<float>(kScreenWidth) / static_cast<float>(kScreenHeight), 0.1f, 100.0f) };
+		shader.use();
+		shader.setMat4("model", model);
+		shader.setMat4("view", view);
+		shader.setMat4("projection", projection);
+		shader.setVec3("viewPos", camera.Position);
+		shader.setVec3("lightPos", light_pos);
+		shader.setBool("blinn", blinn);
+
+		glBindVertexArray(vertex.planeVAO);
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, floor_texture);
+		vertex.Draw(vertex.planeVAO);
+
+		glBindVertexArray(0);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	shader.Clean();
+}
+
+
+int InitWindow() {
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+	window = glfwCreateWindow(kScreenWidth, kScreenHeight, "Advanced Lighting: Blinn-Phong", nullptr, nullptr);
+	if (!window) {
+		std::cout << "Failed to create GLFW window" << std::endl;
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	// init glad
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cout << "Failed to init GLAD" << std::endl;
+		return -1;
+	}
+
+	glEnable(GL_DEPTH_TEST);
+	// enable following line to allow us to modify point size by set gl_PointSize in vertex shader
+	//glEnable(GL_PROGRAM_POINT_SIZE);
+
+	// glEnable(GL_MULTISAMPLE); // enabled by default on some drivers, but not all so always enable to make sure
+
+	glfwSetFramebufferSizeCallback(window, FramebufferSizeCallback);
+	glfwSetCursorPosCallback(window, MouseCallback);
+	glfwSetScrollCallback(window, ScrollCallback);
+
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+	return 0;
+}
+
+unsigned int LoadTexture(const char* file_path) {
+	unsigned int textureID;
+	glGenTextures(1, &textureID);
+
+	int width, height, nrComponents;
+	unsigned char* data = stbi_load(file_path, &width, &height, &nrComponents, 0);
+	if (data) {
+		GLenum format { GL_RGB };
+		if (nrComponents == 1)
+			format = GL_RED;
+		else if (nrComponents == 3)
+			format = GL_RGB;
+		else if (nrComponents == 4)
+			format = GL_RGBA;
+
+		glBindTexture(GL_TEXTURE_2D, textureID);
+		glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+
+		// use GL_CLAMP_TO_EDGE to prevent semi-transparent borders. Due to interpolation it takes texels from next repeat 
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, format == GL_RGBA ? GL_CLAMP_TO_EDGE : GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, format == GL_RGBA ? GL_CLAMP_TO_EDGE : GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+		stbi_image_free(data);
+	} else {
+		std::cout << "Texture failed to load at path: " << file_path << std::endl;
+		stbi_image_free(data);
+	}
+
+	return textureID;
+}
+
+void ProcessInput(GLFWwindow* window) {
+	if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, true);
+	}
+
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		camera.ProcessKeyboard(kForward, delta_time);
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		camera.ProcessKeyboard(kBackward, delta_time);
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		camera.ProcessKeyboard(kLeft, delta_time);
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		camera.ProcessKeyboard(kRight, delta_time);
+
+	if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS) {
+		blinn = !blinn;
+		if (blinn) {
+			std::cout << "Blinn-Phong" << std::endl;
+		} else {
+			std::cout << "Phong" << std::endl;
+		}
+	}
+
+	if (glfwGetKey(window, GLFW_KEY_X) == GLFW_PRESS)
+		camera.ProcessKeyboard(kUpward, delta_time);
+	if (glfwGetKey(window, GLFW_KEY_Z) == GLFW_PRESS)
+		camera.ProcessKeyboard(kDownward, delta_time);
+	if (glfwGetKey(window, GLFW_KEY_P) == GLFW_PRESS)
+		std::cout << "camera pos: { "
+		<<  camera.Position.x << ", " << camera.Position.y << "," << camera.Position.z << " }"
+		<< std::endl;
+}
+
+void FramebufferSizeCallback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
+}
+
+// glfw: whenever the mouse moves, this callback is called
+// -------------------------------------------------------
+void MouseCallback(GLFWwindow* window, double xposIn, double yposIn)
+{
+	float xpos = static_cast<float>(xposIn);
+	float ypos = static_cast<float>(yposIn);
+
+	if (first_mouse)
+	{
+		last_x = xpos;
+		last_y = ypos;
+		first_mouse = false;
+	}
+
+	float xoffset = xpos - last_x;
+	float yoffset = last_y - ypos; // reversed since y-coordinates go from bottom to top
+
+	last_x = xpos;
+	last_y = ypos;
+
+	camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+// glfw: whenever the mouse scroll wheel scrolls, this callback is called
+// ----------------------------------------------------------------------
+void ScrollCallback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	camera.ProcessMouseScroll(static_cast<float>(yoffset));
+}
