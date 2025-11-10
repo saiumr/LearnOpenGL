fbe25ca2c61b3d1739fbf841c135ab3431b87da5
saiumr <colaloveanan@gmail.com>
2025-11-10 11:02:08 +0800

    gl34-Deferred Shading: optimize (by light volumn).
    GPU会执行if所有分支然后丢弃不满足条件的分支，所以这个优化反而可能会使性能更堪忧

--- OpenGL34-DeferredRendering/main.cpp
+++ OpenGL34-DeferredRendering/main.cpp
@@ -183,10 +183,15 @@ void RenderLoop() {
 			shader_lighting_pass.setVec3("lights[" + std::to_string(i) + "].Position", lightPositions[i]);
 			shader_lighting_pass.setVec3("lights[" + std::to_string(i) + "].Color", lightColors[i]);
 			// update attenuation parameters and calculate radius
+			const float constant = 1.0f; // note that we don't send this to the shader, we assume it is always 1.0 (in our case)
 			const float linear = 0.7f;
 			const float quadratic = 1.8f;
 			shader_lighting_pass.setFloat("lights[" + std::to_string(i) + "].Linear", linear);
 			shader_lighting_pass.setFloat("lights[" + std::to_string(i) + "].Quadratic", quadratic);
+			// then calculate radius of light volume/sphere
+			const float maxBrightness = std::fmaxf(std::fmaxf(lightColors[i].r, lightColors[i].g), lightColors[i].b);
+			float radius = (-linear + std::sqrt(linear * linear - 4 * quadratic * (constant - (256.0f / 5.0f) * maxBrightness))) / (2.0f * quadratic);
+			shader_lighting_pass.setFloat("lights[" + std::to_string(i) + "].Radius", radius);
 		}
 		shader_lighting_pass.setVec3("viewPos", camera.Position);
 		glBindVertexArray(vertex.quadVAO);
